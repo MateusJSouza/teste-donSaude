b6835e72e7241a0d63ec34378532b26a7ff0d361
MateusJSouza <mateusjdev@gmail.com>
2023-10-17 19:35:23 -0300

    feat: :sparkles: creation of the NavItem component

---
+++ front/src/app/view/components/Sidebar/NavItem/index.tsx
@@ -0,0 +1,28 @@
+import { CaretDown } from '@phosphor-icons/react'
+import { ElementType } from 'react'
+
+interface NavItemProps {
+  title: string
+  icon: ElementType
+  isSelect?: boolean
+}
+
+export function NavItem({ title, isSelect, icon: Icon }: NavItemProps) {
+  return (
+    <a
+      href="#"
+      className="group flex w-full items-center gap-3 rounded-2xl px-4 py-2 transition-colors hover:bg-primary"
+    >
+      <Icon className="h-5 w-5 text-zinc-500 group-hover:text-white" />
+      <span className="text-xs font-medium text-zinc-500 transition-colors group-hover:font-semibold group-hover:text-white">
+        {title}
+      </span>
+      {isSelect && (
+        <CaretDown
+          size={10}
+          className="text-zinc-500 transition-colors group-hover:text-white"
+        />
+      )}
+    </a>
+  )
+}
