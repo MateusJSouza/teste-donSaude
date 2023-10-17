d35a58c324a5de5310a08f77311fac09caf8cc0e
MateusJSouza <mateusjdev@gmail.com>
2023-10-17 19:36:32 -0300

    feat: :sparkles: creating the Header component

---
+++ front/src/app/view/components/Header.tsx
@@ -0,0 +1,47 @@
+import {
+  BellSimple,
+  MagnifyingGlass,
+  PlusCircle,
+  Star,
+  User,
+} from '@phosphor-icons/react'
+
+import * as Input from '../components/Input'
+
+import { Button } from './Button'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between bg-white px-12 py-6">
+      <div className="flex items-center justify-start gap-4">
+        <Button variant="secondary">
+          <PlusCircle />
+          <span className="font-inter text-sm font-semibold leading-5">
+            Novo Atendimento
+          </span>
+        </Button>
+
+        <Input.Root>
+          <Input.Prefix>
+            <MagnifyingGlass className="h-5 w-5 text-tertiary" />
+          </Input.Prefix>
+          <Input.Control placeholder="Buscar" />
+        </Input.Root>
+      </div>
+
+      <div className="flex items-center justify-center gap-4">
+        <div className="flex items-center gap-8">
+          <Star className="cursor-pointer" />
+
+          <BellSimple className="cursor-pointer" />
+        </div>
+
+        <div className="h-8 w-px bg-zinc-200" />
+
+        <div className="flex cursor-pointer items-center justify-center rounded-full border border-transparent bg-primary-light p-2">
+          <User className="text-primary" />
+        </div>
+      </div>
+    </header>
+  )
+}
