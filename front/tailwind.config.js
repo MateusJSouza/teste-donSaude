c1e2645e520d08c8c961483cedc8b7c61ed7152a
MateusJSouza <mateusjdev@gmail.com>
2023-10-16 22:23:06 -0300

    feat: :lipstick: configuring font and code formatting corrections

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+    },
   },
   plugins: [],
 }
-
