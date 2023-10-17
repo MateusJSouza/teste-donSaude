9ef8abe30bf49625ac2de840f9c1715fffc1cd2b
MateusJSouza <mateusjdev@gmail.com>
2023-10-17 19:31:19 -0300

    feat: :lipstick: adding fonts and style variables

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,11 +3,27 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      body: ['Roboto', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
     },
     extend: {
+      width: {
+        1.5: '10px',
+      },
+      padding: {
+        3.5: '14px',
+      },
       gridTemplateColumns: {
-        app: 'minmax(18rem, 20rem) 1fr',
+        app: '14rem 1fr',
+      },
+      colors: {
+        primary: '#D0005E',
+        'primary-light': '#FFF0F7',
+        content: '#101828',
+        screen: '#F2F4F7',
+        tertiary: '#667085',
+        'shape-secondary': '#EAECF0',
+        secondary: '#475467',
       },
     },
   },
