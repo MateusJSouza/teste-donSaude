eb1c5ae2da955a21ece48c00dc0f8a3bc61d12ff
MateusJSouza <mateusjdev@gmail.com>
2023-10-17 19:34:13 -0300

    feat: :sparkles: add footer icon and application logo

---
+++ front/src/app/view/components/icons/IconFooter.tsx
@@ -0,0 +1,16 @@
+export function IconFooter() {
+  return (
+    <svg
+      width={26}
+      height={26}
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width={26} height={26} rx={13} fill="#D0005E" />
+      <g fill="#fff">
+        <path d="M15.353 10.647V5h-.86c-2.124 0-3.846 1.997-3.846 4.46v1.187h4.706zM15.353 16.54v-1.187h-4.706V21h.86c2.125 0 3.846-1.998 3.846-4.46zM5 10.647h12.838c1.747 0 3.162 1.279 3.162 2.855v1.85H8.162C6.415 15.353 5 14.075 5 12.499v-1.851z" />
+      </g>
+    </svg>
+  )
+}
