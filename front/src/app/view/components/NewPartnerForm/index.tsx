80b09d311b6b14b2ff38a42ebfde7835b7e7495a
MateusJSouza <mateusjdev@gmail.com>
2023-10-18 20:38:02 -0300

    feat: :sparkles: adding new partner form and creating default layout

---
+++ front/src/app/view/components/NewPartnerForm/index.tsx
@@ -0,0 +1,105 @@
+import { Button } from '../Button'
+import * as Input from '../Input'
+import { Label } from '../Label'
+import * as Select from '../Select'
+import { SelectWithDropdown } from '../SelectWithDropdown'
+
+export function NewPartnerForm() {
+  return (
+    <form className="mt-6 font-inter">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col">
+          <Label>Categoria</Label>
+
+          <Select.Root name="category">
+            <Select.Trigger>
+              <Select.Value placeholder="Selecione uma categoria" />
+            </Select.Trigger>
+
+            <Select.Content>
+              <Select.Item value="category-1">
+                <Select.ItemText>Categoria 1</Select.ItemText>
+              </Select.Item>
+
+              <Select.Item value="category-2">
+                <Select.ItemText>Categoria 2</Select.ItemText>
+              </Select.Item>
+
+              <Select.Item value="category-3">
+                <Select.ItemText>Categoria 3</Select.ItemText>
+              </Select.Item>
+            </Select.Content>
+          </Select.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>CNPJ</Label>
+
+          <Input.Root rounded>
+            <Input.Control placeholder="Digite o CNPJ" />
+          </Input.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>Nome</Label>
+
+          <Input.Root rounded>
+            <Input.Control placeholder="Digite o nome completo" />
+          </Input.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>E-mail</Label>
+
+          <Input.Root rounded>
+            <Input.Control type="email" placeholder="Digite seu e-mail" />
+          </Input.Root>
+        </div>
+
+        <SelectWithDropdown />
+        <SelectWithDropdown />
+
+        <div className="flex flex-col">
+          <Label>Responsável Clínico</Label>
+          <Input.Root rounded>
+            <Input.Control placeholder="Digite o nome completo" />
+          </Input.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>Responsável Financeiro</Label>
+
+          <Input.Root rounded>
+            <Input.Control placeholder="Digite o nome completo" />
+          </Input.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>Senha</Label>
+
+          <Input.Root rounded>
+            <Input.Control type="password" placeholder="Digite sua senha" />
+          </Input.Root>
+        </div>
+
+        <div className="flex flex-col">
+          <Label>Confirme a senha</Label>
+
+          <Input.Root rounded>
+            <Input.Control
+              type="password"
+              placeholder="Digite novamente aqui"
+            />
+          </Input.Root>
+        </div>
+      </div>
+
+      <div className="mt-8 flex items-center justify-end gap-3">
+        <Button variant="ghost" className="px-20 py-[10px]">
+          Cancelar
+        </Button>
+        <Button className="px-[94px] py-[10px]">Próxima</Button>
+      </div>
+    </form>
+  )
+}
---
+++ front/src/app/view/layouts/PartnerLayout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom'
+
+import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
+
+export function PartnerLayout() {
+  return (
+    <div className="min-h-screen md:grid md:grid-cols-app">
+      <Sidebar />
+
+      <main className="flex flex-col">
+        <Header />
+
+        <div className="flex-grow rounded-2xl bg-shape-secondary px-12 pb-4 pt-8 sm:px-6 sm:pt-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  )
+}
