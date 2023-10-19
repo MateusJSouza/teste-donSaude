19f704398b6889730fd34ab206e20e963c6176f2
MateusJSouza <mateusjdev@gmail.com>
2023-10-19 15:36:10 -0300

    refactor: :truck: renaming layout file

--- front/src/app/Router/index.tsx
+++ front/src/app/Router/index.tsx
@@ -3,16 +3,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ListPartners } from '../view/components/ListPartners'
 import { PartnerFormAddress } from '../view/components/PartnerFormAddress'
 import { PartnerFormInfo } from '../view/components/PartnerFormInfo'
-import { PartnerLayout } from '../view/layouts/PartnerLayout'
+import { ListPartnerLayout } from '../view/layouts/ListPartnerLayout'
+import { PartnerLayoutForm } from '../view/layouts/PartnerLayoutForm'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<PartnerLayout />}>
+        <Route element={<ListPartnerLayout />}>
           <Route path="/" element={<ListPartners />} />
           <Route path="/new" element={<PartnerFormInfo />} />
           <Route path="/address" element={<PartnerFormAddress />} />
+
+          <Route element={<PartnerLayoutForm />}>
+            <Route path="/info" element={<PartnerFormInfo />} />
+            <Route path="/address" element={<PartnerFormAddress />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
