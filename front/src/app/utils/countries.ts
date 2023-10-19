2871abe1f9db1b87cd239b108cc98ba5ac44d02a
MateusJSouza <mateusjdev@gmail.com>
2023-10-19 19:03:07 -0300

    feat: :clown_face: adding countries mock data

---
+++ front/src/app/utils/countries.ts
@@ -0,0 +1,23 @@
+export const countries = {
+  BR: '+55', // Brasil
+  US: '+1', // Estados Unidos
+  UK: '+44', // Reino Unido
+  CA: '+1', // Canadá
+  AU: '+61', // Austrália
+  DE: '+49', // Alemanha
+  FR: '+33', // França
+  JP: '+81', // Japão
+  IT: '+39', // Itália
+  ES: '+34', // Espanha
+  MX: '+52', // México
+  AR: '+54', // Argentina
+  PT: '+351', // Portugal
+  IN: '+91', // Índia
+  CN: '+86', // China
+  RU: '+7', // Rússia
+  ZA: '+27', // África do Sul
+  NG: '+234', // Nigéria
+  EG: '+20', // Egito
+  SA: '+966', // Arábia Saudita
+  AE: '+971', // Emirados Árabes Unidos
+}
